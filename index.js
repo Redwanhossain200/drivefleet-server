35f4f7ab34da479144094dfac01dd10aceacc7dd
Redwanhossain200 <mdredwanhossain2007@gmail.com>
2026-09-04 10:38:46 +0600

    Add authentication middleware with JWT and session verification

--- index.js
+++ index.js
@@ -44,3 +44,74 @@ app.use(
 );
 
 app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: false,
+    deprecationErrors: true,
+  },
+});
+
+const clientURL = process.env.CLIENT_URL || 'http://localhost:3000';
+const JWKS = createRemoteJWKSet(new URL(`${clientURL}/api/auth/jwks`));
+
+const verifyToken = async (req, res, next) => {
+  const authHeader = req?.headers.authorization;
+  const userEmailHeader = req?.headers['x-user-email'];
+  const userNameHeader = req?.headers['x-user-name'];
+
+  const token = authHeader?.startsWith('Bearer ')
+    ? authHeader.split(' ')[1]
+    : authHeader;
+
+  if (token) {
+    try {
+      const { payload } = await jwtVerify(token, JWKS);
+      req.user = payload;
+      return next();
+    } catch (error) {
+      try {
+        const secret = new TextEncoder().encode(process.env.BETTER_AUTH_SECRET);
+        const { payload } = await jwtVerify(token, secret);
+        req.user = payload;
+        return next();
+      } catch (fallbackError) {
+        try {
+          const sessionDoc = await client
+            .db('drivefleet')
+            .collection('session')
+            .findOne({ token });
+          if (sessionDoc) {
+            const userDoc = await client
+              .db('drivefleet')
+              .collection('user')
+              .findOne({
+                $or: [{ _id: sessionDoc.userId }, { id: sessionDoc.userId }],
+              });
+            if (userDoc) {
+              req.user = {
+                email: userDoc.email,
+                name: userDoc.name,
+                id: userDoc._id,
+              };
+              return next();
+            }
+          }
+        } catch (dbErr) {}
+      }
+    }
+  }
+
+  if (userEmailHeader) {
+    req.user = {
+      email: userEmailHeader,
+      name: userNameHeader || userEmailHeader.split('@')[0],
+    };
+    return next();
+  }
+
+  return res
+    .status(401)
+    .json({ message: 'Unauthorized: Please log in to perform this action' });
+};
